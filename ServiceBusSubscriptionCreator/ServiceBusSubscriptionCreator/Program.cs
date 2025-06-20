5a66d541628627ed861a5129e71932ab7fc08d29
MOD Administrator <admin@MngEnvMCAP732343.onmicrosoft.com>
2025-06-20 14:11:30 +0700

    20/6

---
+++ ServiceBusSubscriptionCreator/ServiceBusSubscriptionCreator/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+
+public class Project
+{
+    private const string topicConnectionString = "Endpoint=sb://srvbusstandard.servicebus.windows.net/;SharedAccessKeyName=connection;SharedAccessKey=73jnWHoPwRFGvg+LDEVmo+ey1ur1ZQPRv+ASbFcDV8Y=;EntityPath=firsttopic";
+    private const string topicName = "firsttopic";
+    private const string subscriptionName = "subs3";
+    public static async Task Main(string[] args)
+    {
+        var adminClient = new ServiceBusAdministrationClient(topicConnectionString);
+        await adminClient.CreateSubscriptionAsync(topicName, subscriptionName);
+        Console.WriteLine("Subscription created!");
+    }
+}
